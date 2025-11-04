2bbf00775bced1a2de2b9cf48c9e60b55788a3b9
Vipul Sharma <vipuls181999@gmail.com>
2025-11-04 12:39:18 -0600

    update execution permission, entrypoint, and cmd for docker containers

---
+++ torchao_float8/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+export HF_HOME=/root/.cache/huggingface
+export HF_HUB_ENABLE_HF_TRANSFER=1
+
+source /workspace/torchao_float8/.venv/bin/activate
+
+echo "[entrypoint] Running prepare script from common_utils."
+bash /workspace/common_utils/scripts/prepare.sh
+echo "[entrypoint] entrypoint script complete"
+
+# Only exec if arguments are provided
+if [ $# -gt 0 ]; then
+    echo "[entrypoint] Running exec with arguments: $@"
+    exec "$@"
+else
+    echo "[entrypoint] No arguments provided, script completed successfully"
+fi
+
